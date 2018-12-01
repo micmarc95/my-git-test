15c5a8a1b65cccd685463d6ab42f17b12cc6c190
Michał <1.michal.marcinkowski@gmail.com>
2018-12-01 14:16:28 +0100

    [add] - remove Car class, add Dog

--- src/main/java/pl/marcinkowski/my_git_test/Car.java
+++
@@ -1,7 +0,0 @@
-package pl.marcinkowski.my_git_test;
-
-public class Car {
-
-    public Car() {
-    }
-}
---
+++ src/main/java/pl/marcinkowski/my_git_test/Dog.java
@@ -0,0 +1,8 @@
+package pl.marcinkowski.my_git_test;
+
+public class Dog extends Animal{
+
+    public Dog(String name) {
+        super(name);
+    }
+}
