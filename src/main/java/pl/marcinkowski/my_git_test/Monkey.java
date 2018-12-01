6999728666c086d2c460553c74bee7a5a9f84e29
Michał <1.michal.marcinkowski@gmail.com>
2018-12-01 14:47:36 +0100

    [change] - change monkey

--- src/main/java/pl/marcinkowski/my_git_test/Monkey.java
+++ src/main/java/pl/marcinkowski/my_git_test/Monkey.java
@@ -4,4 +4,9 @@ public class Monkey extends Animal {
     public Monkey(String name) {
         super(name);
     }
+
+    @Override
+    public String toString() {
+        return "Monkey{" + '}';
+    }
 }
